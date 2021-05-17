f4d3be8ad052d18c87eda77a190eeb62feb3f38d
jsakamoto <junichi.jp@gmail.com>
2021-05-18 06:40:44 +0900

    Suppress all error messages output into a browser console.

--- HandleExceptionGlobally/Program.cs
+++ HandleExceptionGlobally/Program.cs
@@ -4,6 +4,8 @@ using System.Threading.Tasks;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
 
 namespace HandleExceptionGlobally
 {
@@ -13,6 +15,9 @@ namespace HandleExceptionGlobally
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            // See also: https://docs.microsoft.com/en-us/aspnet/core/blazor/fundamentals/configuration?view=aspnetcore-5.0#logging-configuration
+            builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredToast();
