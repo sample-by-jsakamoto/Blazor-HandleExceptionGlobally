b10639dc6968f2c644c3da7c3699d8c97667c5a2
jsakamoto <junichi.jp@gmail.com>
2021-05-18 06:09:42 +0900

    Initial commit.

---
+++ HandleExceptionGlobally/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Blazored.Toast;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HandleExceptionGlobally
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredToast();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
